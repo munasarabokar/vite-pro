5bda3c9386b937af62816a1a75414310ee0e6047
Munasar abuukar <munasarabokar@gmail.com>
2023-06-01 16:40:44 +0300

    added website

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { splitVendorChunkPlugin } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-pro/",
+  base: "/",
   plugins: [react(), splitVendorChunkPlugin()],
   server: {
     port: 3000,
