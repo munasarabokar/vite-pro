68a472d3d6c0a808f455c8b265705bb2d354c897
Munasar abuukar <munasarabokar@gmail.com>
2023-06-02 10:30:02 +0300

    updated website

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://mobile.e-muraad.com",
+        target: "https://mobile.e-muraad.com",
         changeOrigin: true,
       },
     },
